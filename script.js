f99e92d408dd6300a9bb096f8a600ffd2487582d
rahatmaini <rahatmaini@live.com>
2018-12-23 16:24:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ function onScroll() {
 const $$header = document.querySelector('.js-header');
 
 window.addEventListener('scroll', throttle(onScroll, 25));
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+      
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+   } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+  
